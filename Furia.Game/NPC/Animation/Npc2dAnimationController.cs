42ee60dab899a8c84a8fa54e9bc3836592546abc
Jaime Arturo <jamessanz@outlook.com>
2025-09-18 18:02:51 -0600

    Initial commit

---
+++ Furia.Game/NPC/Animation/Npc2dAnimationController.cs
@@ -0,0 +1,141 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Rendering.Sprites;
+
+namespace Furia.NPC.Animation
+{
+    public class Npc2dAnimationController : SyncScript
+    {
+        public byte animationSpeed = 12; // I have no idea how to describe how the speed works, but lesser the value, faster the animation
+        public byte idleStartFrame = 0, idleEndFrame = 0;
+        public byte attackStartFrame = 0 , attackEndFrame = 0;
+        public byte walkStartFrame = 0, walkEndFrame = 0;
+        public byte deathStartFrame = 0, deathEndFrame = 0;
+        public byte hitStartFrame = 0, hitEndFrame = 0;
+
+        private byte currentStartFrame = 0, currentEndFrame = 0;
+        private float clock = 0;
+
+        //Animation state machine
+        
+        //Components
+        private SpriteComponent spriteComponent;
+        private SpriteFromSheet spriteSheet;
+
+        public override void Start()
+        {
+            CheckComponents();
+            //This is how you are supposed to set sprite frames https://doc.stride3d.net/4.0/en/manual/sprites/use-sprites.html
+            spriteComponent = Entity.Get<SpriteComponent>();
+            spriteSheet = spriteComponent.SpriteProvider as SpriteFromSheet;
+        }
+
+        public override void Update()
+        {
+        }
+
+        public void PlayIdleAnimation()
+        {
+            currentStartFrame = idleStartFrame;
+            currentEndFrame = idleEndFrame;
+            PlayFrames();
+        }
+
+        public void PlayWalkAnimation()
+        {
+            currentStartFrame = walkStartFrame;
+            currentEndFrame = walkEndFrame;
+            PlayFrames();
+        }
+
+        public void PlayAttackAnimation()
+        {
+            currentStartFrame = attackStartFrame;
+            currentEndFrame = attackEndFrame;
+            PlayFrames();
+        }
+
+        public void PlayDeathAnimation() 
+        {
+            currentStartFrame = deathStartFrame;
+            currentEndFrame = deathEndFrame;
+            PlayFramesNoLoop();
+        }
+
+        public void PlayHitAnimation()
+        {
+            currentStartFrame = hitStartFrame;
+            currentEndFrame = hitEndFrame;
+            PlayFrames();
+        }
+
+        //Plays the frames
+        public void PlayFrames ()
+        {
+            if (Counter())
+            {
+                if (spriteSheet.CurrentFrame > currentEndFrame || spriteSheet.CurrentFrame < currentStartFrame)
+                {
+                    spriteSheet.CurrentFrame = currentStartFrame;
+                }
+
+                if (spriteSheet.CurrentFrame < currentEndFrame)
+                {
+                    spriteSheet.CurrentFrame += 1;
+                }
+                else
+                {
+                    spriteSheet.CurrentFrame = currentStartFrame;
+                }
+            }
+        }
+
+        private bool aux = false;
+        private bool animationFinished = false;
+        public void PlayFramesNoLoop()
+        {
+            if (!aux)
+            {
+                spriteSheet.CurrentFrame = deathStartFrame;
+                aux = true;
+            }
+
+            if (!animationFinished)
+            {
+                if (Counter())
+                {
+                    if (spriteSheet.CurrentFrame < currentEndFrame)
+                    {
+                        spriteSheet.CurrentFrame += 1;
+                    }
+                    else
+                    {
+                        spriteSheet.CurrentFrame = deathEndFrame;
+                        animationFinished = true;
+                    }
+                }
+            }
+        }
+
+        private bool Counter ()
+        {
+            if (clock >=  animationSpeed * 0.01)
+            {
+                clock = 0;
+                return true;
+            }
+
+            clock += 1 * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+
+            return false;
+        }
+
+        private void CheckComponents()
+        {
+            if (spriteComponent == null || spriteSheet == null)
+            {
+                DebugText.Print(Entity.Name + " has null components!!", new Int2(500, 300));
+            }
+        }
+    }
+}
